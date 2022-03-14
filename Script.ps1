ee6122d7ec71081f1a83758123d74376728722bd
EliasGrinwis <r0892566@student.thomasmore.be>
2022-03-14 12:59:24 +0100

    Script Updated

--- Script.ps1
+++ Script.ps1
@@ -10,9 +10,9 @@ $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Authorization", "Bearer AAAAAAAAAAAAAAAAAAAAANELaAEAAAAANsF2y1aOzi1URzYP%2BlBEqm6SasI%3DDZAg2pzCdexxOaxfMhcw8cRRKimZVNlT0EVXaYp9VLBHzKZU0x")
 $headers.Add("Cookie", "ct0=c0b93a352b1ea4fa791226a333803a0b; guest_id=v1%3A164707946344757662")
 
-$response = Invoke-RestMethod "https://api.twitter.com/2/users/by/username/$username" -Method 'GET' -Headers $headers
-$response | ConvertTo-Json 
-
+$responseUsername = Invoke-RestMethod "https://api.twitter.com/2/users/by/username/$username" -Method 'GET' -Headers $headers
+$responseUsername | ConvertTo-Json
+$responseUsername | ConvertTo-Html | Out-File -Append -FilePath C:\Users\Elias\Desktop\Project\index.html
 
 "
 Ok, so here you can find some information about
@@ -27,8 +27,9 @@ $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Authorization", "Bearer AAAAAAAAAAAAAAAAAAAAANELaAEAAAAANsF2y1aOzi1URzYP%2BlBEqm6SasI%3DDZAg2pzCdexxOaxfMhcw8cRRKimZVNlT0EVXaYp9VLBHzKZU0x")
 $headers.Add("Cookie", "ct0=c0b93a352b1ea4fa791226a333803a0b; guest_id=v1%3A164707946344757662")
 
-$response = Invoke-RestMethod "https://api.twitter.com/2/users/$followers/followers" -Method 'GET' -Headers $headers
-$response | ConvertTo-Json
+$responseFollowers = Invoke-RestMethod "https://api.twitter.com/2/users/$followers/followers" -Method 'GET' -Headers $headers
+$responseFollowers | ConvertTo-Json
+$responseFollowers | ConvertTo-Html | Out-File -Append -FilePath C:\Users\Elias\Desktop\Project\index.html
 
 
 "
@@ -49,8 +50,20 @@ if ($choice -eq "yes") {
 
     $body = "{`n    `"text`": `"$tweet`"`n}"
 
-    $response = Invoke-RestMethod 'https://api.twitter.com/2/tweets' -Method 'POST' -Headers $headers -Body $body
-    $response | ConvertTo-Json
+    $responseTweet = Invoke-RestMethod 'https://api.twitter.com/2/tweets' -Method 'POST' -Headers $headers -Body $body
+    $responseTweet | ConvertTo-Json
+    $responseTweet | ConvertTo-Html | Out-File -Append -FilePath C:\Users\Elias\Desktop\Project\index.html
+
 }  else {
-    Write-host "Ok thank you for doing this script"
+    Write-host "Ok thank, now i have one more question"
+}
+
+$CLearContent = Read-Host "Do you want to clear the HTML file? [yes | no]".ToLower()
+if ($CLearContent -eq "yes") {
+    Clear-Content "C:\Users\admin\Project\index.html"
+    Write-Host "Ok all your information in your HTML file is deleted"
+} else {
+    Write-host "Ok thank you for doing this script, you can find all your
+information in the HTML file. Everytime you do this script again the
+information will be added to the index file."
 }
