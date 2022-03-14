1aacd6c660510a5990848fbf671db939ccf1978d
EliasGrinwis <r0892566@student.thomasmore.be>
2022-03-14 13:32:44 +0100

    Script Updated

--- Script.ps1
+++ Script.ps1
@@ -12,7 +12,7 @@ $headers.Add("Cookie", "ct0=c0b93a352b1ea4fa791226a333803a0b; guest_id=v1%3A1647
 
 $responseUsername = Invoke-RestMethod "https://api.twitter.com/2/users/by/username/$username" -Method 'GET' -Headers $headers
 $responseUsername | ConvertTo-Json
-$responseUsername | ConvertTo-Html | Out-File -Append -FilePath C:\Users\Elias\Desktop\Project\index.html
+$responseUsername | ConvertTo-Html | Out-File -Append -FilePath C:\Users\Elias\Desktop\Project\index.html | Format-Table -AutoSize -Property Name, Dependant
 
 "
 Ok, so here you can find some information about
@@ -29,7 +29,7 @@ $headers.Add("Cookie", "ct0=c0b93a352b1ea4fa791226a333803a0b; guest_id=v1%3A1647
 
 $responseFollowers = Invoke-RestMethod "https://api.twitter.com/2/users/$followers/followers" -Method 'GET' -Headers $headers
 $responseFollowers | ConvertTo-Json
-$responseFollowers | ConvertTo-Html | Out-File -Append -FilePath C:\Users\Elias\Desktop\Project\index.html
+$responseFollowers | ConvertTo-Html | Out-File -Append -FilePath C:\Users\Elias\Desktop\Project\index.html | Format-Table -AutoSize -Property Name, Dependant
 
 
 "
@@ -52,7 +52,7 @@ if ($choice -eq "yes") {
 
     $responseTweet = Invoke-RestMethod 'https://api.twitter.com/2/tweets' -Method 'POST' -Headers $headers -Body $body
     $responseTweet | ConvertTo-Json
-    $responseTweet | ConvertTo-Html | Out-File -Append -FilePath C:\Users\Elias\Desktop\Project\index.html
+    $responseTweet | ConvertTo-Html | Out-File -Append -FilePath C:\Users\Elias\Desktop\Project\index.html | Format-Table -Property Name, Dependant
 
 }  else {
     Write-host "Ok thank, now i have one more question"
@@ -60,7 +60,7 @@ if ($choice -eq "yes") {
 
 $CLearContent = Read-Host "Do you want to clear the HTML file? [yes | no]".ToLower()
 if ($CLearContent -eq "yes") {
-    Clear-Content "C:\Users\admin\Project\index.html"
+    Clear-Content "C:\Users\Elias\Desktop\Project\index.html"
     Write-Host "Ok all your information in your HTML file is deleted"
 } else {
     Write-host "Ok thank you for doing this script, you can find all your
