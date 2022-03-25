4c4e7d8142c37be24063af96c96fab8338fc7272
EliasGrinwis <r0892566@student.thomasmore.be>
2022-03-25 15:32:34 +0100

    Script Updated

--- Script.ps1
+++ Script.ps1
@@ -9,72 +9,53 @@ $username = Read-Host "Give me your Twitter account and i will give you your Twi
 
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Authorization", "Bearer AAAAAAAAAAAAAAAAAAAAANELaAEAAAAANsF2y1aOzi1URzYP%2BlBEqm6SasI%3DDZAg2pzCdexxOaxfMhcw8cRRKimZVNlT0EVXaYp9VLBHzKZU0x")
-$headers.Add("Cookie", "ct0=c0b93a352b1ea4fa791226a333803a0b; guest_id=v1%3A164707946344757662")
+$headers.Add("Cookie", "guest_id=v1%3A164788172340091642")
 
-$responseUsername = Invoke-RestMethod "https://api.twitter.com/2/users/by/username/$username" -Method 'GET' -Headers $headers
-$responseUsername | ConvertTo-Json
-$responseUsername | ConvertTo-Json | Out-File -Append -FilePath C:\Users\Elias\Desktop\Project\index.html
+$response = Invoke-RestMethod "https://api.twitter.com/2/users/by/username/$username" -Method 'GET' -Headers $headers
+$response | ConvertTo-Json
+$response | ConvertTo-Html | Out-File C:\Users\Elias\Desktop\Project\IndexTest.html -Append -Encoding ascii
 
- "
+"
  Ok, so here you can find some information about
  your twitter account, you can also see your id.
  You need this id for the following question.
  "
 
  #Dit gaat de volgers van een bepaalde user weergeven
- $followers = Read-Host "Give me your Twitter id and i will give you a list of your followers"
+ $questionFollowers = Read-Host "Do you want to get a list of your followers? [yes | no]"
+ if ($questionFollowers -eq "yes") {
+    $followers = Read-Host "Give me your Twitter id and i will give you a list of your followers"
 
- $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
- $headers.Add("Authorization", "Bearer AAAAAAAAAAAAAAAAAAAAANELaAEAAAAANsF2y1aOzi1URzYP%2BlBEqm6SasI%3DDZAg2pzCdexxOaxfMhcw8cRRKimZVNlT0EVXaYp9VLBHzKZU0x")
- $headers.Add("Cookie", "ct0=c0b93a352b1ea4fa791226a333803a0b; guest_id=v1%3A164707946344757662")
+    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $headers.Add("Authorization", "Bearer AAAAAAAAAAAAAAAAAAAAANELaAEAAAAANsF2y1aOzi1URzYP%2BlBEqm6SasI%3DDZAg2pzCdexxOaxfMhcw8cRRKimZVNlT0EVXaYp9VLBHzKZU0x")
+    $headers.Add("Cookie", "ct0=c0b93a352b1ea4fa791226a333803a0b; guest_id=v1%3A164707946344757662")
 
- $responseFollowers = Invoke-RestMethod "https://api.twitter.com/2/users/$followers/followers" -Method 'GET' -Headers $headers
- $responseFollowers | ConvertTo-Json
- $responseFollowers | ConvertTo-Html | Out-File -Append -FilePath C:\Users\Elias\Desktop\Project\blad.csv
+    $responseFollowers = Invoke-RestMethod "https://api.twitter.com/2/users/$followers/followers" -Method 'GET' -Headers $headers
+    $responseFollowers | ConvertTo-Json
+    $responseFollowers | ConvertTo-Html | Out-File C:\Users\Elias\Desktop\Project\IndexTest.html -Append -Encoding ascii
 
-
- "
+"
  Ok so now you have a list of your followers but do you maybe
  want to make a tweet to your twitter account using a script?
  "
+ } else { Write-Host "Ok, I have one more question"
+ }
 
-
-# #hier ga we de user vrage wat hij wil tweeten op zijn account
- $choice = Read-Host "Do you want to make a TWEET? [yes | no]".ToLower()
+ $choice = Read-Host "Do you want to make a TWEET? [yes | no]"
  if ($choice -eq "yes") {
      $tweet = Read-Host "What do you want to TWEET?"
+        $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+        $headers.Add("Authorization", "OAuth oauth_consumer_key=`"rpam0SB4T9eO92nKJ6PLCsAzp`",oauth_token=`"1488143836597657605-fMCqsqotB2hEgklYeJEbUyJXqoC4Sn`",oauth_signature_method=`"HMAC-SHA1`",oauth_timestamp=`"1648214851`",oauth_nonce=`"e7vsTkX0IGT`",oauth_version=`"1.0`",oauth_signature=`"WWvwurKRoW2w7BAbD4s5fbeUGJ0%3D`"")
+        $headers.Add("Content-Type", "application/json")
+        $headers.Add("Cookie", "guest_id=v1%3A164787151794347198")
 
-     $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-     $headers.Add("Authorization", "OAuth oauth_consumer_key=`"rpam0SB4T9eO92nKJ6PLCsAzp`",oauth_token=`"1488143836597657605-fMCqsqotB2hEgklYeJEbUyJXqoC4Sn`",oauth_signature_method=`"HMAC-SHA1`",oauth_timestamp=`"1647702974`",oauth_nonce=`"EZ5h7g90VtD`",oauth_version=`"1.0`",oauth_signature=`"uFOx03KLB%2BecdBzkbmC8ljodrmM%3D`"")
-     $headers.Add("Content-Type", "application/json")
-     $headers.Add("Cookie", "guest_id=v1%3A164707946344757662")
-     
-     $body = "{`n    `"text`": `"$tweet`"`n}"
-     
-     $response = Invoke-RestMethod 'https://api.twitter.com/2/tweets' -Method 'POST' -Headers $headers -Body $body
-     $response | ConvertTo-Json
+        $body = "{`n    `"text`": `"$tweet`"`n}"
+
+        $responseTweet = Invoke-RestMethod 'https://api.twitter.com/2/tweets' -Method 'POST' -Headers $headers -Body $body
+        $responseTweet | ConvertTo-Json 
+        $responseTweet | ConvertTo-Html | Out-File C:\Users\Elias\Desktop\Project\IndexTest.html -Append -Encoding ascii
+
  }  else {
      Write-host "
- Ok thank, now i have one more question"
+ Thankyou for doing my script. You can run this script everytime you want!!"
  }
-
- $CLearContent = Read-Host "
-Do you want to clear the HTML file? [yes | no]".ToLower()
- if ($CLearContent -eq "yes") {
-     Clear-Content "C:\Users\Elias\Desktop\Project\index.html"
-     Write-Host "
- Ok all your information in your HTML file is deleted"
- } else {
-     Write-host "
- Ok thank you for doing this script, you can find all your
- information in the HTML file. Everytime you do this script again the
- information will be added to the index file.
- "
- }
-
- "
- Ok thank you for doing this script, you can find all your
- information in the HTML file. Everytime you do this script again the
- information will be added to the index file.
- "
-
